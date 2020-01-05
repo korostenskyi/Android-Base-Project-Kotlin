e1df1c49efc912773ab015214a388ccaa774d7e0
Roman Korostenskyi <romankorostensky@gmail.com>
2020-01-05 19:00:41 +0200

    feat: add InternalLogger

--- app/src/main/java/com/korostenskyi/androidbaseproject/ui/base/ui/ViewModelBottomSheetDialogFragment.kt
+++ app/src/main/java/com/korostenskyi/androidbaseproject/ui/base/ui/ViewModelBottomSheetDialogFragment.kt
@@ -12,6 +12,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.korostenskyi.androidbaseproject.R
 import com.korostenskyi.androidbaseproject.ui.base.viewModel.BaseViewModel
+import com.korostenskyi.utils.InternalLogger
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
@@ -31,15 +32,18 @@ abstract class ViewModelBottomSheetDialogFragment<V : BaseViewModel>(
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onCreateView")
         return inflater.inflate(layoutId, container, false)
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onCreateDialog")
         return BottomSheetDialog(requireContext(), theme)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onCreate")
         AndroidSupportInjection.inject(this)
         viewModel = ViewModelProvider(this, vmFactory).get(vmClass)
     }
@@ -47,11 +51,13 @@ abstract class ViewModelBottomSheetDialogFragment<V : BaseViewModel>(
     @CallSuper
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onViewCreated")
         viewModel.onViewCreated()
     }
 
     override fun onStart() {
         super.onStart()
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onStart")
         dialog?.window?.apply {
             setBackgroundDrawable(null)
             setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
@@ -62,17 +68,20 @@ abstract class ViewModelBottomSheetDialogFragment<V : BaseViewModel>(
 
     override fun onResume() {
         super.onResume()
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onResume")
         viewModel.onResume()
     }
 
     override fun onPause() {
         super.onPause()
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onPause")
         viewModel.onPause()
     }
 
     @CallSuper
     override fun onDestroyView() {
         super.onDestroyView()
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onDestroyView")
         viewModel.onDestroyView()
     }
 }
--- app/src/main/java/com/korostenskyi/androidbaseproject/ui/base/ui/ViewModelFragment.kt
+++ app/src/main/java/com/korostenskyi/androidbaseproject/ui/base/ui/ViewModelFragment.kt
@@ -7,6 +7,7 @@ import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.korostenskyi.androidbaseproject.ui.base.viewModel.BaseViewModel
+import com.korostenskyi.utils.InternalLogger
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
@@ -21,6 +22,7 @@ abstract class ViewModelFragment<V : BaseViewModel>(
     @CallSuper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onCreate")
         AndroidSupportInjection.inject(this)
         viewModel = ViewModelProvider(this, vmFactory).get(vmClass).apply {
             onCreate()
@@ -30,41 +32,48 @@ abstract class ViewModelFragment<V : BaseViewModel>(
     @CallSuper
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onViewCreated")
         viewModel.onViewCreated()
     }
 
     @CallSuper
     override fun onStart() {
         super.onStart()
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onStart")
         viewModel.onStart()
     }
 
     @CallSuper
     override fun onResume() {
         super.onResume()
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onResume")
         viewModel.onResume()
     }
 
     @CallSuper
     override fun onPause() {
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onPause")
         viewModel.onPause()
         super.onPause()
     }
 
     @CallSuper
     override fun onStop() {
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onStop")
         viewModel.onStop()
         super.onStop()
     }
 
     @CallSuper
     override fun onDestroyView() {
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onDestroyView")
         viewModel.onDestroyView()
         super.onDestroyView()
     }
 
     @CallSuper
     override fun onDestroy() {
+        InternalLogger.lifecycleEvent("${javaClass.simpleName} onDestroy")
         viewModel.onDestroy()
         super.onDestroy()
     }
