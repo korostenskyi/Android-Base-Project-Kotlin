9ea32ce52e09a5929ad0460fdcd3b9af8cf6f289
Roman Korostenskyi <romankorostensky@gmail.com>
2020-01-05 16:34:56 +0200

    refactor: remove old stuff
    remove Koin
    remove useless utils
    updated dependencies

--- app/src/main/java/com/korostenskyi/androidbaseproject/App.kt
+++ app/src/main/java/com/korostenskyi/androidbaseproject/App.kt
@@ -1,17 +1,5 @@
 package com.korostenskyi.androidbaseproject
 
 import android.app.Application
-import com.korostenskyi.androidbaseproject.di.loginModule
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
 
-class App: Application() {
-    override fun onCreate() {
-        super.onCreate()
-
-        startKoin {
-            androidContext(this@App)
-            modules(loginModule)
-        }
-    }
-}
+class App: Application()
